6f185c8785eb5f5da50b9a79dbb1fed34f1765e1
CornelI5 <kenzoyooo0@gmail.com>
2026-07-05 09:01:34 +0700

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,28 +1,32 @@
 #include "memory.h"
-#include "theia_log.h" 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include "theia_log.h"
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-_Noreturn static void oom_panic(size_t size, const char *file, int line) {
-    int err = errno;
-    
-    THEIA_LOG(LOG_CRITICAL, "OOM Panic at %s:%d | Requested: %zu bytes, Reason: %s", 
-              file, line, size, strerror(err));
+void *theia_map_phys(uintptr_t phys_addr, size_t size) {
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1) {
+        THEIA_LOG(LOG_CRITICAL, "Failed to open /dev/mem. Need root privileges.");
+        exit(EXIT_FAILURE);
+    }
 
-#ifdef DEBUG
-    abort();
-#else
-    exit(EXIT_FAILURE);
-#endif
+    size_t page_size = sysconf(_SC_PAGESIZE);
+    uintptr_t offset = phys_addr % page_size;
+    uintptr_t aligned_addr = phys_addr - offset;
+
+    void *ptr = mmap(NULL, size + offset, PROT_READ | PROT_WRITE, MAP_SHARED, fd, aligned_addr);
+    close(fd);
+
+    if (ptr == MAP_FAILED) {
+        THEIA_LOG(LOG_ERROR, "Mapping 0x%lx failed", phys_addr);
+        return NULL;
+    }
+
+    THEIA_LOG(LOG_INFO, "Mapped 0x%lx to virtual address %p", phys_addr, ptr + offset);
+    return (void *)((uintptr_t)ptr + offset);
 }
 
-void *safe_malloc_impl(size_t size, const char *file, int line) {
-    void *ptr = malloc(size);
-    if (!ptr) {
-        oom_panic(size, file, line);
-    }
-    THEIA_LOG(LOG_INFO, "Memory allocated: %zu bytes at %p", size, ptr);
-    return ptr;
+void theia_unmap_phys(void *virt_addr, size_t size) {
+    munmap(virt_addr, size); 
 }
