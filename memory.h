44bebbf2c2f000475b9aaf30beede159a379de89
CornelI5 <kenzoyooo0@gmail.com>
2026-07-05 08:20:01 +0700

    Update memory.h

--- memory.h
+++ memory.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+/* Macro to capture file and line information for debugging */
 #define SAFE_MALLOC(x) safe_malloc_impl(((x)), __FILE__, __LINE__)
 
 void *safe_malloc_impl(size_t size, const char *file, int line);
