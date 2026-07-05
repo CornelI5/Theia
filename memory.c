271b843bc9e448ceaca077990e1adc825b24b0dc
CornelI5 <kenzoyooo0@gmail.com>
2026-07-05 08:57:18 +0700

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,14 +1,16 @@
 #include "memory.h"
+#include "theia_log.h" 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <stdnoreturn.h>
 
 _Noreturn static void oom_panic(size_t size, const char *file, int line) {
     int err = errno;
-    fprintf(stderr, "FATAL: Memory allocation failed at %s:%d\n", file, line);
-    fprintf(stderr, "Requested: %zu bytes, Reason: %s\n", size, strerror(err));
+    
+    THEIA_LOG(LOG_CRITICAL, "OOM Panic at %s:%d | Requested: %zu bytes, Reason: %s", 
+              file, line, size, strerror(err));
+
 #ifdef DEBUG
     abort();
 #else
@@ -18,6 +20,9 @@ _Noreturn static void oom_panic(size_t size, const char *file, int line) {
 
 void *safe_malloc_impl(size_t size, const char *file, int line) {
     void *ptr = malloc(size);
-    if (!ptr) oom_panic(size, file, line);
+    if (!ptr) {
+        oom_panic(size, file, line);
+    }
+    THEIA_LOG(LOG_INFO, "Memory allocated: %zu bytes at %p", size, ptr);
     return ptr;
 }
