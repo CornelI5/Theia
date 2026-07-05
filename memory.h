e18e007489249bc0d6ba8fb3632acb43653f231e
CornelI5 <kenzoyooo0@gmail.com>
2026-07-05 08:59:59 +0700

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,16 +1,13 @@
-/*
- * Theia Compiler v0.1 - Memory Management
- * Licensed under MIT License
- */
 #ifndef MEMORY_H
 #define MEMORY_H
 
 #include <stddef.h>
-
+#include <stdint.h>
 
 void *safe_malloc_impl(size_t size, const char *file, int line);
+#define SAFE_MALLOC(s) safe_malloc_impl((s), __FILE__, __LINE__)
 
-
-#define SAFE_MALLOC(size) safe_malloc_impl((size), __FILE__, __LINE__)
+void *theia_map_phys(uintptr_t phys_addr, size_t size);
+void theia_unmap_phys(void *virt_addr, size_t size);
 
 #endif
